eac652ba1bd3c455bfa08545467f8d2b14907ee3
egijm <136075160+egijm@users.noreply.github.com>
2024-03-30 15:03:46 +0700

    Add files via upload

---
+++ NILAI/config.php
@@ -0,0 +1,13 @@
+<?php
+    $host = 'localhost';
+    $userdb = 'root';
+    $passdb = '';
+    $db = 'NILAI';
+    $con = mysqli_connect($host, $userdb, $passdb, $db);
+
+   if(mysqli_connect_errno()){
+    echo "Kesalahan Koneksi : " . mysqli_connect_error();
+    exit();
+}   
+
+
